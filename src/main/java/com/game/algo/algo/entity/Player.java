ad8acbd0c6f3fec270769c1553e216ce27a90e22
GoF9490 <osis9490@gmail.com>
2023-09-06 01:31:25 +0900

    refactor : Player 엔티티 리팩토링. Player의 설계와 다르게 쓰이거나 필요없어진 변수 및 메서드 정리, 이에 맞춰 테스트코드 일부 수정 및 삭제

--- src/main/java/com/game/algo/algo/entity/Player.java
+++ src/main/java/com/game/algo/algo/entity/Player.java
@@ -41,17 +41,9 @@ public class Player {
 
     private Integer drawBlockIndexNum = -1;
 
-    // 필요한가? 아래 트리거를 키는 용도로 쓰였는데 아래 변수가 사라지면 필요성이 정밀한 검증용도로만 쓸 수 있을텐데 이 또한 게임 로직을 정밀하게 탐색하기는 구조적으로 힘들다.
-    // 그렇다고 안전장치를 아예 없애기에는 게임 핵등에 취약할듯. (쓸사람이 있겠냐 싶지만)
-    // 아래 boolean 변수를 없애고 JokerRange 로만 한정적인 검증을 하는게 좋을지도?
-    private Integer whiteJokerRange; // startNum * 100 + endNum
+    private Integer whiteJokerRange = 12; // startNum * 100 + endNum
 
-    private Integer blackJokerRange; // startNum * 100 + endNum
-
-    // 필요한가? 원래는 트리거로 쓰려했는데 검증용도로만 쓰인다.
-    private boolean needWhiteJokerRelocation = false;
-
-    private boolean needBlackJokerRelocation = false;
+    private Integer blackJokerRange = 12; // startNum * 100 + endNum
 
 
     public static Player create(String name, String webSocketSessionId) {
@@ -75,8 +67,8 @@ public class Player {
     public void gameReset() {
         orderNumber = 0;
         blockList = new ArrayList<>();
-        whiteJokerRange = null;
-        blackJokerRange = null;
+        whiteJokerRange = 12;
+        blackJokerRange = 12;
     }
 
     public int findPosition(Block drawBlock) {
@@ -92,39 +84,41 @@ public class Player {
     }
 
     public void addBlock(Block block) {
-        distinguishJoker(block);
-        exploreJokerRange(block);
-
         int indexNum = findPosition(block);
         blockList.add(indexNum, block);
         setDrawBlockIndexNum(block);
     }
 
     public void changeJokerNum(int index, BlockColor jokerColor) {
+        Block findDrawBlock = getDrawBlock();
         Block findJoker = findJoker(jokerColor);
         blockList.remove(findJoker);
+
+        if (!betweenRange(findDrawBlock, findDrawBlock.isColor(BlockColor.WHITE) ? whiteJokerRange : blackJokerRange)) {
+            throw new GameLogicException(GameExceptionCode.JOKER_NOT_MATCH);
+        }
 
         int frontNum = (index == 0) ? 0 : blockList.get(index - 1).getNum();
         int backNum = (index >= blockList.size()) ? 12 : blockList.get(index).getNum();
 
-        if (jokerColor == BlockColor.WHITE && needWhiteJokerRelocation) {
+        if (jokerColor == BlockColor.WHITE) {
             whiteJokerRange = frontNum * 100 + backNum;
-            needWhiteJokerRelocation = false;
-        } else if (jokerColor == BlockColor.BLACK && needBlackJokerRelocation) {
+        } else if (jokerColor == BlockColor.BLACK) {
             blackJokerRange = frontNum * 100 + backNum;
-            needBlackJokerRelocation = false;
-        } else {
-            throw new GameLogicException(GameExceptionCode.ALREADY_EXECUTED);
         }
 
         blockList.add(index, findJoker);
 
         blockList = new ArrayList<>(blockList);
-        setDrawBlockIndexNum(findJoker);
+        setDrawBlockIndexNum(findDrawBlock);
     }
 
     public void updateOrder(int order) {
         this.orderNumber = order;
+    }
+
+    public Block getDrawBlock() {
+        return blockList.get(drawBlockIndexNum);
     }
 
     public boolean guessBlock(int index, int num) {
@@ -153,28 +147,11 @@ public class Player {
         return findJoker.get(0);
     }
 
-    private void exploreJokerRange(Block block) {
-        if (whiteJokerRange != null && block.isColor(BlockColor.WHITE) && betweenRange(block, whiteJokerRange)) {
-            needWhiteJokerRelocation = true;
-        } else if (blackJokerRange != null && block.isColor(BlockColor.BLACK) && betweenRange(block, blackJokerRange)) {
-            needBlackJokerRelocation = true;
-        }
-    }
-
     private boolean betweenRange(Block block, int jokerRange) {
-        return jokerRange / 100 <= block.getNum() && block.getNum() < jokerRange % 100;
-    }
-
-    private void distinguishJoker(Block block) {
         if (block.isJoker()) {
-            if (block.isColor(BlockColor.WHITE)) {
-                whiteJokerRange = 12;
-                needWhiteJokerRelocation = true;
-            } else {
-                blackJokerRange = 12;
-                needBlackJokerRelocation = true;
-            }
+            return true;
         }
+        return jokerRange / 100 <= block.getNum() && block.getNum() <= jokerRange % 100;
     }
 
     private void setDrawBlockIndexNum(Block block) {
