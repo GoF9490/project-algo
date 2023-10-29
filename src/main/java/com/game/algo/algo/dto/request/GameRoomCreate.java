d10eb2fde267d1f151c8b335f8ccdaebe13d42ef
GoF9490 <osis9490@gmail.com>
2023-10-30 00:05:29 +0900

    fix : request dto 필드값 valid 수정

--- src/main/java/com/game/algo/algo/dto/request/GameRoomCreate.java
+++ src/main/java/com/game/algo/algo/dto/request/GameRoomCreate.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -14,7 +15,8 @@ public class GameRoomCreate {
 
     private Long playerId;
 
-    @Size(min = 3, max = 15 ,message = "DisplayName의 길이는 3이상 15이하여야 합니다.")
+    @NotBlank(message = "닉네임의 길이는 3이상 15이하여야 합니다.")
+    @Size(min = 3, max = 15 ,message = "닉네임의 길이는 3이상 15이하여야 합니다.")
     @Pattern(regexp = "^[ㄱ-ㅎ|ㅏ-ㅣ|가-핳|a-z|A-Z|0-9]+$", message = "한글, 숫자, 알파벳만 사용 가능합니다.")
     private String title;
 }
